d8dd0dde1d1b6d94d79811aa27eacae80c23261a
Adrian Velasquez <141940483+adrianvelasquezs@users.noreply.github.com>
2026-02-18 18:52:09 -0500

    feat: add configuration and TypeORM setup for PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const stage = process.env.STAGE ?? process.env.NODE_ENV ?? 'dev';
+const isProd = stage === 'prod' || stage === 'production';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: !isProd,
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
