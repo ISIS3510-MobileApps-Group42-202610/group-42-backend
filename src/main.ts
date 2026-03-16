bee04a6c9a6d70dc1233b88f6dcc25b522e7d04f
Adrian Velasquez <141940483+adrianvelasquezs@users.noreply.github.com>
2026-03-16 13:25:47 -0500

    Change to check register on iOS app

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   app.enableCors();
